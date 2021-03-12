7573d60d3a15b40cc2f0320b7901c9832f0dcc34
Yemliha <yemlihanist@gmail.com>
2021-03-12 12:07:24 +0300

    Flag Quiz App
    Quiz application I developed using sqlite

---
+++ flag quiz app/BayrakQuizUygulamasi/app/src/main/java/com/info/flagquizapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.info.flagquizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.info.sqlitekullanimihazirveritabani.DatabaseCopyHelper
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        veritabaniKopyala()
+
+        buttonBasla.setOnClickListener {
+
+            startActivity(Intent(this@MainActivity,QuizActivity::class.java))
+
+        }
+    }
+
+    fun veritabaniKopyala(){
+
+        val copyHelper = DatabaseCopyHelper(this)
+
+        try{
+            copyHelper.createDataBase()
+            copyHelper.openDataBase()
+
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+
+    }
+}
